cc4fd4c18ea5500354a8ad2c587664b581ea6138
sadhu <rong8296@gmail.com>
2015-02-16 14:31:14 +0800

    添加访问日志功能

--- solitude.js
+++ solitude.js
@@ -1,6 +1,7 @@
 var fs = require("fs"),
     express = require("express"),
     yml = require("js-yaml"),
+    morgan = require("morgan"),
     solitude = express(),
     cwd = process.cwd(),
     conf = yml.safeLoad(fs.readFileSync(cwd + "/conf.yml")),
@@ -10,8 +11,23 @@ var fs = require("fs"),
 fs.readdir(routesDir, function(err, routerFiles) {
     if(err) throw err;
 
-    var routers = [],
-        preDir = routesDir + "/";
+    var preDir = routesDir + "/",
+        logsDir = cwd + conf.logsDir,
+        logPath = logsDir + "/access.log",
+        routers = [];
+
+    fs.mkdir(logsDir, function(err) {
+        if(err && err.code === "EEXIST") return;
+        else throw err;
+        
+        fs.open(logPath, "a", function(err, fd) {fs.close(fd);});
+    });
+    solitude.use(morgan(
+        ":remote-addr _ :remote-user _ :date[iso] _ :method _ :url _ :http-version _ :status _ :res[content-length] _ :response-time _ :referrer _ :user-agent",
+        {
+            stream: fs.createWriteStream(logPath, {flags: "a"})
+        }
+    ));
 
     routerFiles.forEach(function(fileName) {
         routers.push(require(preDir + fileName.split(".")[0]));
