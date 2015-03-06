e4add303779f9eeb3fba7ae3cb80c70f6065caad
sadhu <rong8296@gmail.com>
2015-03-06 17:34:40 +0800

    使用 swig 输出内容

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var index = require("express")();
 index.route(["/", "/index"])
         .get(function(req, res) {
-            res.send("index page");
+            res.status(200).type("html").render("index", {
+                title: "首页",
+                date: new Date()
+            });
         });
 module.exports = index;
--- solitude.js
+++ solitude.js
@@ -1,6 +1,7 @@
 var fs = require("fs"),
     yml = require("js-yaml"),
     logger = require("./utils/logger"),
+    swig = require("swig"),
     router = require("./utils/router"),
     solitude = require("express")(),
     cwd = process.cwd(),
@@ -10,6 +11,11 @@ var fs = require("fs"),
 
 // morgan in logger
 solitude.use(logger(cwd, conf));
+
+// siwg template
+solitude.engine("swig", swig.renderFile);
+solitude.set("view engine", "swig");
+
 solitude.use(router(cwd, conf));
 
 solitude.listen(8000);
