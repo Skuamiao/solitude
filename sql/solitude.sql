0a7ffd7df3078b45349bea95758f562a7a240201
sadhu <rong8296@gmail.com>
2015-03-18 17:21:32 +0800

    增加 sha1 函数和 pgcrypto 库

--- sql/solitude.sql
+++ sql/solitude.sql
@@ -1,5 +1,20 @@
-create table if not exists authors (
-    id vachar(6),
+-- 丢弃所有表
+drop table if exists authors cascade;
+
+-- 丢弃所有扩展
+drop extension if exists pgcrypto cascade;
+
+create extension pgcrypto;
+
+create or replace function sha1(text) returns text as
+$$
+    select encode(digest($1, 'sha1'), 'hex')
+$$
+language sql strict immutable;
+
+-- 建 authors 表
+create table authors (
+    id varchar(6),
     email varchar(27),
     passowrd char(40),
     name varchar(13),
