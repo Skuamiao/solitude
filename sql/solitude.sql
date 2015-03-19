304fc1ed8d7408109570ce9600815258ff7c2e8f
sadhu <rong8296@gmail.com>
2015-03-19 17:48:14 +0800

    学习 postgresql 服务器端编程

--- sql/solitude.sql
+++ sql/solitude.sql
@@ -4,19 +4,36 @@ drop table if exists authors cascade;
 -- 丢弃所有扩展
 drop extension if exists pgcrypto cascade;
 
-create extension pgcrypto;
+-- 丢弃所有函数
+drop function if exists sha1(text) cascade;
+-- drop function if exists gen_id() cascade;
 
-create or replace function sha1(text) returns text as
+
+create extension pgcrypto;
+/*
+create function sha1(text)
+returns bytea as
 $$
-    select encode(digest($1, 'sha1'), 'hex')
+begin
+    return digest($1, 'sha1');
+end
+$$
+language plpgsql strict immutable;
+*/
+/*
+create function gen_id() returns integer as
+$$
+
 $$
 language sql strict immutable;
+*/
 
 -- 建 authors 表
 create table authors (
-    id varchar(6),
-    email varchar(27),
-    passowrd char(40),
-    name varchar(13),
-    avatar text
+    id varchar(6) primary key,
+    email varchar(27) not null,
+    passowrd bytea not null,
+    name varchar(13) default '',
+    avatar text default '',
+    created timestamp with time zone not null
 );
