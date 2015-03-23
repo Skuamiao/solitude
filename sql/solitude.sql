09d1290e3a773e07e6f29a41c983021c44bbfbf0
sadhu <rong8296@gmail.com>
2015-03-23 17:02:53 +0800

    当记录插入 authors 表时，自动生成随机整数和有时区的当前日期时间

--- sql/solitude.sql
+++ sql/solitude.sql
@@ -5,35 +5,42 @@ drop table if exists authors cascade;
 drop extension if exists pgcrypto cascade;
 
 -- 丢弃所有函数
-drop function if exists sha1(text) cascade;
--- drop function if exists gen_id() cascade;
+drop function if exists gen_id() cascade;
 
 
 create extension pgcrypto;
-/*
-create function sha1(text)
-returns bytea as
-$$
-begin
-    return digest($1, 'sha1');
-end
-$$
-language plpgsql strict immutable;
-*/
-/*
-create function gen_id() returns integer as
-$$
 
-$$
-language sql strict immutable;
+/*
+    1. 生成一个 6 位随机数
+    2. 确认没有存在过; 如果存在，重复 1，2；如果不存在 3
+    3. 使用这个数
 */
+create function gen_id() returns integer as $$
+declare
+    aid integer;
+    rid integer;
+begin
+    loop
+        -- 1
+        select round(random() * 1000000) into aid;
+        if(aid < 100000) then
+            select aid + 100000 into aid;
+        end if;
+        -- 2
+        exit when not exists(select id from authors where aid = id);
+    end loop;
+    -- 4
+    return aid;
+end;
+$$ language plpgsql strict;
+
 
 -- 建 authors 表
 create table authors (
-    id varchar(6) primary key,
-    email varchar(27) not null,
-    passowrd bytea not null,
+    id integer default gen_id() primary key,
+    email varchar(27) not null unique,
+    password bytea not null,
     name varchar(13) default '',
     avatar text default '',
-    created timestamp with time zone not null
+    created timestamp with time zone default current_timestamp
 );
