8d91481f40b8cf953ceadbeb7cc13d5d9315e4e9
sadhu <rong8296@gmail.com>
2015-03-31 21:33:19 +0800

    错误描述和规则脚本除错

--- api/rules.js
+++ api/rules.js
@@ -9,13 +9,13 @@ module.exports = {
         return 5 < l && l < 17 ? true : "2000";
     },
     isPwdSame: function(str1, str2) {
-        return str1 === str2 ？ true : "2001";
+        return str1 === str2 ? true : "2001";
     },
     isNameNotEmpty: function(str) {
         return str.trim().length ? true : "3000";
     },
     isNameInLen: function(str) {
-        return str.trim().length < 18 ？ true : "3001";
+        return str.trim().length < 18 ? true : "3001";
     },
     isMobile: function(str) {
         return /\d{11}/g.test(str.trim());
