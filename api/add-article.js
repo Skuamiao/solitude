f34c3d800a1b76bfe95635a01a310be35487493d
sadhu <rong8296@gmail.com>
2015-05-05 17:44:28 +0800

    处理文章发布，实现 markdown 格式的转换

--- api/add-article.js
+++ api/add-article.js
@@ -1,7 +1,29 @@
 module.exports = function addArticle(api) {
     api.route("/add-article").post(function(req, res) {
+        var lhl = require("highlight.js"),
+            mi = require("markdown-it")({
+                langPrefix: "hljs ",
+                highlight: function (str, lang) {
+                    if (lang && lhl.getLanguage(lang))
+                        try {
+                            // console.log("conf", lhl.highlight(lang, str).value);
+                            return lhl.highlight(lang, str).value;
+                        } catch (__) {}
+
+                    try {
+                        // console.log("auto", lhl.highlightAuto(str).value);
+                        return lhl.highlightAuto(str).value;
+                    } catch (__) {}
+
+                    return "";
+                }
+            });
         if(res.locals.authenticated)
-            res.end(req.body.at);
+            res.status(200).type("html").render("pages/add-article", {
+                title: "管理",
+                date: new Date(),
+                md: mi.render(req.body.at)
+            });
         else
             res.redirect("/");
     });
