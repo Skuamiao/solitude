8104824336167b406b6924560b966c8c5b74b3f5
sadhu <rong8296@gmail.com>
2015-08-18 14:33:10 +0800

    验证码图片不再生成图片，而是直接返回图片的 buffer

--- routes/api/verify.js
+++ routes/api/verify.js
@@ -1,15 +1,14 @@
 module.exports = function(api) {
     var imageMagick = require('gm').subClass({imageMagick: true});
     api.route('/verify').get(function(req, res) {
-        imageMagick(process.cwd() + '/assets/images/code-base.png').fill('#53c619').fontSize(14).drawText(3, 16,'1 2 3 4').swirl(-59).write(process.cwd() + '/assets/images/test.png', function(err) {
-            if(err) throw err;
-            res.sendFile(
-                'test.png', 
-                {root: process.cwd() + '/assets/images'}, 
-                function (err) {
-                    if (err) throw err;
-                }
-            );
-        });
+        imageMagick(process.cwd() + '/assets/images/code-base.png')
+            .fill('#53c619')
+            .fontSize(14)
+            .drawText(3, 16,' 12 34')
+            .swirl(-83)
+            .toBuffer('png', function(err, buf) {
+                if(err) throw err;
+                res.send(buf);
+            });
     });
 };
