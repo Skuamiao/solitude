df7eb2a0bdfb805d577116b8636e367c7aff75b0
sadhu <rong8296@gmail.com>
2015-08-18 18:00:04 +0800

    session research

--- routes/manager/sign-up.js
+++ routes/manager/sign-up.js
@@ -1,6 +1,20 @@
 module.exports = function(manager) {
-    var React = require('react');
-    manager.route("/sign-up").get(function(req, res) {
+    var React = require('react'),
+        session = require("express-session"),
+        RedisStore = require("connect-redis")(session),
+        store = new RedisStore({ttl: 120}),
+        sess = session({
+            secret: "ciklid",
+            resave: false,
+            saveUninitialized: false,
+            store: store,
+            rolling: true
+        });
+    manager.route("/sign-up").get(sess, function(req, res) {
+        console.log(req.sessionID, req.session);
+        store.set(req.sessionID, req.session, function(err) {
+            if(err) throw err;
+        });
         res.render('sign-up', {
             formSignUp: React.renderToString(
                 React.createFactory(require('../../jsxes/sign-up'))()
