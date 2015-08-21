2db2df3bf8d971c42b1a882408c74ebd462f330d
sadhu <rong8296@gmail.com>
2015-08-21 15:14:35 +0800

    little change

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,6 @@
 module.exports = function(apps) {
-    var nodes = require('./router.json'),
-        keys = Object.keys(nodes);
-    keys.forEach(function(key) {
+    var nodes = require('./router.json');
+    Object.keys(nodes).forEach(function(key) {
         var routes = nodes[key],
             app = apps[key];
         routes.forEach(function(route) {
