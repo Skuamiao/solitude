d5428c8e223ac01db0d64397b0a2ba58f53f5073
sadhu <rong8296@gmail.com>
2015-09-09 18:06:52 +0800

    try to add verify

--- routes/api/verify.js
+++ routes/api/verify.js
@@ -1,6 +1,22 @@
 module.exports = function(api) {
-    var imageMagick = require('gm').subClass({imageMagick: true});
-    imageMagick('./assets/images/code-base.png')
+    var imageMagick = require('gm').subClass({imageMagick: true}),
+        session = require("express-session"),
+        RedisStore = require("connect-redis")(session),
+        store = new RedisStore({ttl: 120}),
+        sess = session({
+            secret: "ciklid",
+            resave: false,
+            name: '_<',
+            saveUninitialized: false,
+            store: store,
+            cookie: {
+                path: '/'
+            },
+            rolling: true
+        });
+    api.route('/verify').get(sess, function(req, res) {
+        console.log(req.sessionID, req.session);
+        imageMagick('./assets/images/code-base.png')
         .fill('#53c619')
         .fontSize(14)
         .drawText(3, 16,'1 2 3 4')
@@ -8,15 +24,19 @@ module.exports = function(api) {
         .toBuffer('png', function(err, buf) {
             if(err) {
                 console.log(err);
-                api.route('/verify').get(function(req, res) {
-                    res.sendFile('verify-demo.png', {
-                        root: './assets/images'
-                    });
+                res.sendFile('verify-demo.png', {
+                    root: './assets/images'
                 });
             }else {
-                api.route('/verify').get(function(req, res) {
-                    res.send(buf);
+                store.set(req.sessionID, req.session, function(err) {
+                    if(err) {
+                        console.log(err);
+                    }else {
+                        res.send(buf);
+                    }
                 });
             }
         });
+    });
+    
 };
