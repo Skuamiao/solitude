aedcf1a7ea7bfdebfb2d061e32ba9432d3522544
sadhu <rong8296@gmail.com>
2016-03-06 16:53:29 +0800

    simulate https

--- solitude.js
+++ solitude.js
@@ -1,10 +1,46 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var solitude = express();
+//
+// solitude.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+//
+// solitude.listen(8008, function () {
+//   console.log('Example solitude listening on port 8008!');
+// });
 
-app.get('/', function (req, res) {
+var tls = require('tls'),
+    fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
+    express = require('express'),
+    solitude = express(),
+    options = {
+      key: fs.readFileSync('private-key.pem'),
+      cert: fs.readFileSync('public-cert.pem')
+    };
+
+solitude.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8008, function () {
-  console.log('Example app listening on port 8008!');
+// http.createServer(solitude).listen(8080);
+
+/*
+* Using https module
+*/
+
+https.createServer(options, solitude).listen(8008, function() {
+  console.log('https server listening on port ' + 8008);
 });
+
+
+
+/*
+* Using tls module
+*/
+// tls.createServer(options, function (cleartextStream) {
+//   cleartextStream.write("welcome!n");
+//   cleartextStream.setEncoding('utf8');
+//   cleartextStream.end();
+// }).listen(8080);
